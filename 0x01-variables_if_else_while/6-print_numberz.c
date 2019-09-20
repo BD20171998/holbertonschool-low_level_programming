49481bcdd4984808957cc8c49c136c49f6a9f265
Bob Deprizio <robert.deprizio@gmail.com>
2019-09-20 00:50:00 +0000

    6. Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Program that prints numbers 0-9 using "putchar"
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+		putchar(i);
+
+	putchar(10);
+
+	return (0);
+}
