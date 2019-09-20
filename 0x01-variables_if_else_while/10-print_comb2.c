a1d10f0ab2661a16a44c085992c150e5b0ab1f9c
Bob Deprizio <robert.deprizio@gmail.com>
2019-09-20 02:39:32 +0000

    10. 00...99

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Program that prints numbers 0-99
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 48; i < 58; i++)
+	{
+		for (j = 48; j < 58; j++)
+		{
+			putchar(i);
+			putchar(j);
+
+			if (j == 57)
+				break;
+
+			putchar(44);
+
+			putchar(32);
+		}
+
+		if (j == 57 && i == 57)
+			break;
+		putchar(44);
+
+		putchar(32);
+	}
+
+	putchar(10);
+
+	return (0);
+}
