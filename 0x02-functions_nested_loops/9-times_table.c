5d7f354ee692983c5e743579eafc0f3d315822b1
Bob Deprizio <robert.deprizio@gmail.com>
2019-09-24 22:58:12 +0000

    9. Learn your times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+
+/**
+ * times_table - function that prints the 9 times table, starting with 0
+ *
+ * Return: void
+ */
+
+void times_table(void)
+{
+	int d, i, j, k;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			k = j * i;
+			d = k / 10;
+
+			if (d > 0)
+			{
+				_putchar(d + '0');
+				_putchar(k % 10 + '0');
+			}
+
+			else
+				_putchar(k + '0');
+
+			if (j == 9)
+				break;
+			_putchar(',');
+			_putchar(' ');
+
+		}
+
+		_putchar('\n');
+	}
+}
