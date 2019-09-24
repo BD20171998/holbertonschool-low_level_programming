07d3b5fc1f5bcf030b75fb4ce1524420276025dd
Bob Deprizio <robert.deprizio@gmail.com>
2019-09-24 20:51:28 +0000

    1. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * print_alphabet - writes the lower case alphabet to stdout
+ *
+ * Return: void
+ */
+
+void  print_alphabet(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		_putchar(i);
+
+	_putchar('\n');
+}
