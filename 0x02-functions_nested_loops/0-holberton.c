519ea4084ac5feee6f91534c53d6f8e7bb20c1cc
Bob Deprizio <robert.deprizio@gmail.com>
2019-09-24 20:45:54 +0000

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * main - Program that prints string
+ * Return: 0
+ */
+
+int main(void)
+{
+	char mystring[9] = "Holberton";
+
+	int i;
+
+	for (i = 0; i < 10; i++)
+		_putchar(mystring[i]);
+
+	_putchar('\n');
+
+	return (0);
+}
