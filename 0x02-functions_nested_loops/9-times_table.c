bb3f0c89a1fb1a12571d960675a0d62828f161fc
Bob Deprizio <robert.deprizio@gmail.com>
2019-09-25 02:25:31 +0000

    9. Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,37 +13,34 @@ void times_table(void)
 	for (i = 0; i < 10; i++)
 	{
 
-		for (j = 0; j < 10; j++)
+
+				_putchar('0');
+
+
+		for (j = 1; j < 10; j++)
 		{
 
 			k = j * i;
 			d = k / 10;
 
-			if (d > 0)
+			if ((d > 0) && (k > 0))
 			{
-				_putchar(d + '0');
-				_putchar(k % 10 + '0');
-
-				if (j == 9)
-					break;
-
 				_putchar(',');
 				_putchar(' ');
+				_putchar(d + '0');
+				_putchar(k % 10 + '0');
 			}
 
 			else
 			{
-				_putchar(k + '0');
-
-				if (j == 9)
-					break;
-
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
+				_putchar(k + '0');
 			}
 
 		}
+
 			_putchar('\n');
 
 	}
