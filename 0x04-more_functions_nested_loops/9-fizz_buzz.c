a5e264c7005694ab9e08a8ff0d7140cd01509683
Bob Deprizio <robert.deprizio@gmail.com>
2019-09-26 20:50:03 +0000

    9. Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+	int i;
+
+	for (i = 1; i < 101; ++i)
+	{
+
+		if((i % 3) == 0 && (i % 5) == 0)
+			printf("FizzBuzz");
+
+		else if((i % 3) == 0)
+			printf("Fizz");
+
+		else if((i % 5) == 0)
+			printf("Buzz");
+
+		else
+			printf("%i", i);
+
+		if (i == 100)
+			break;
+
+		putchar(' ');
+	}
+	return (0);
+}
