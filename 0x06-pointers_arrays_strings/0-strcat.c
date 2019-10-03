29c6179812863a740285bcb673e8b02118e13d13
Bob Deprizio <robert.deprizio@gmail.com>
2019-10-03 18:46:52 +0000

    0. strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+
+/**
+ * _strcat - function that concatenates two strings
+ * including the terminating null byte (\0), to the buffer pointed to by dest
+ * @dest: char pointer to be extended
+ * @src: char pointer to be copied
+ * Return: char pointer to be returned
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i = 0, length = 0, length2 = 0;
+
+	while (*(src + i) != '\0')
+	{
+		++length;
+		++i;
+	}
+
+	i = 0;
+
+	while (*(dest + i) != '\0')
+	{
+		dest[i]	= *(dest + i);
+		++length2;
+		++i;
+	}
+
+	for (i = 0; src[i] != '\0'; ++i)
+		dest[length2 + i] = src[i];
+
+	dest[length2 + length] = '\0';
+
+	return (dest);
+
+}
---
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -0,0 +1,6 @@
+#ifndef _holberton_
+#define _holberton_
+int _putchar(char c);
+char *_strcat(char *dest, char *src);
+
+#endif
