e22b22f3e9b65af4d30b7bd8a1a775497b445d02
Bob Deprizio <robert.deprizio@gmail.com>
2019-10-03 19:02:03 +0000

    1. strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+
+/**
+ * _strcat - function that concatenates two strings up to n bytes
+ * to the buffer pointed to by dest
+ * @dest: char pointer to be extended
+ * @src: char pointer to be copied
+ * @n: bytes to be appended from copied string
+ * Return: char pointer to be returned
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0, length = 0, length2 = 0;
+
+	while (*(src + i) != '\0')
+	{
+		++length;
+		++i;
+	}
+
+	i = 0;
+
+	while (*(dest + i) != '\0')
+	{
+		dest[i] = *(dest + i);
+		++length2;
+		++i;
+	}
+
+	for (i = 0; i < n; ++i)
+		dest[length2 + i] = src[i];
+
+	dest[length2 + n] = '\0';
+
+	return (dest);
+
+}
