59b91123373ad4b4f3295f5e465338f981d7843e
Bob Deprizio <robert.deprizio@gmail.com>
2019-10-03 20:36:18 +0000

    2. strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * _strncpy - function that copies one string to another up to n bytes
+ * @dest: char pointer to receive copy up to n bytes
+ * @src: char pointer to be copied
+ * @n: bytes to be copied
+ * Return: char pointer to be returned
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0, length = 0;
+
+	while (*(src + i) != '\0')
+	{
+		++length;
+		++i;
+	}
+
+	for (i = 0; i < length; ++i)
+		dest[i] = src[i];
+
+	for (i = length; i < n; ++i)
+		dest[i] = '\0';
+
+	return (dest);
+
+}
