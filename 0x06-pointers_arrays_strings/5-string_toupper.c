b5774d4a87c45cd03a790a1b4002719e877703e2
Bob Deprizio <robert.deprizio@gmail.com>
2019-10-04 00:59:59 +0000

    5. Always look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * string_toupper - function that changes all lowercase letters of a string
+ * to uppercase
+ * @c: char pointer to convert
+ * Return: char pointer that will return string as upper case
+ */
+
+char *string_toupper(char *c)
+{
+	int i = 0;
+
+	while (*(c + i) != '\0')
+	{
+		if ((*(c + i) >= 97) && (*(c + i) <= 122))
+			*(c + i) -= 32;
+
+		++i;
+	}
+
+	return (c);
+}
