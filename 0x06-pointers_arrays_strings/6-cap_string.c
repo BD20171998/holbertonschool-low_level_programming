dba18a702576a44b503e48fd0fe573e632157b57
Bob Deprizio <robert.deprizio@gmail.com>
2019-10-04 04:09:21 +0000

    6. Expect the best. Prepare for the worst. Capitalize on what comes

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * cap_string - function that capitalizes all words of a string
+ * @c: char pointer to be possibly converted
+ * Return: char pointer with converted string
+ */
+
+char *cap_string(char *c)
+{
+	int i = 0, j;
+
+	char sep[13] = {'\t', '\n', 32, 33, 34, 40, 41, 44, 46, 59, 63, 123,
+			125};
+
+	while (*(c + i) != '\0')
+	{
+		for (j = 0; j < 14; ++j)
+		{
+			if (*(c + i) == sep[j] &&  (*(c + i + 1) >= 97 &&
+						     *(c + i + 1) <= 122))
+			{
+				*(c + i + 1) -= 32;
+				break;
+			}
+		}
+
+		++i;
+	}
+	return (c);
+}
