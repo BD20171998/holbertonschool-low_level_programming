6d705abf37d37325d4fe8898a0826170c929b837
Bob Deprizio <robert.deprizio@gmail.com>
2019-10-09 19:27:42 +0000

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -14,6 +14,6 @@ void _puts_recursion(char *s)
 		_puts_recursion((s + 1));
 	}
 
-	if (*(s + 1) == '\0')
+	if (*s == '\0')
 		_putchar('\n');
 }
---
+++ 0x08-recursion/holberton.h
@@ -0,0 +1,6 @@
+#ifndef _holberton_
+#define _holberton_
+int _putchar(char c);
+void _puts_recursion(char *s);
+
+#endif
