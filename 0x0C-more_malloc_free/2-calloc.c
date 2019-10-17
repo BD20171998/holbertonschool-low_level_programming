73816d22be95ce9f8155405fe4e4999fc1a673aa
Bob Deprizio <robert.deprizio@gmail.com>
2019-10-17 23:58:56 +0000

    2. _calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -23,7 +23,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (a == NULL)
 		return (NULL);
 
-	for (i = 0; i < nmemb; i++)
+	for (i = 0; i < (nmemb * size); i++)
 		*((unsigned char *) a) = 0;
 
 	return (a);
