09be8462b750193620ce1b62bfdc2ee5ba6953f8
Bob Deprizio <robert.deprizio@gmail.com>
2019-10-17 21:05:21 +0000

    5. It isn't the mountains ahead to climb that wear you out; it's the pebble in your shoe

--- 0x0B-malloc_free/5-argstostr.c
+++ 0x0B-malloc_free/5-argstostr.c
@@ -2,6 +2,20 @@
 #include <stdlib.h>
 
 /**
+ * _strlen_recursion - function that returns the length of a string
+ * @s: char pointer for string
+ * Return: int for length of string
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s)
+		return (_strlen_recursion(s + 1) + 1);
+
+	return (0);
+}
+
+/**
  * argstostr - function that concatenates all the arguments of your program
  * @ac: int for number of arguments
  * @av: char double pointer for array of arguments
