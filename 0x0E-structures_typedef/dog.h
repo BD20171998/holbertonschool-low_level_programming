e9a5c34913d95cdf3ff36b8324dd5cace50d3bed
Bob Deprizio <robert.deprizio@gmail.com>
2019-10-21 23:41:44 +0000

    0. Django

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef _dog_
+#define _dog_
+
+/**
+ * struct dog - Dog elements
+ * @name: Char pointer
+ * @age: Float for age
+ * @owner: Char pointer for owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+#endif
