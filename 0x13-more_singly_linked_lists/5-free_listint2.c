31a1165fe2a6111a2c33215217091d8c72b5f21a
Bob Deprizio <robert.deprizio@gmail.com>
2019-11-05 18:19:37 +0000

    5. Free

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - function that frees a listint_t list
+ * @head: Const double pointer of structure listint_t for beginning
+ * Return: Void
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	while (*head != NULL)
+	{
+		temp = *head;
+
+		*head = (*head)->next;
+		free(temp);
+	}
+
+	*head = NULL;
+
+}
