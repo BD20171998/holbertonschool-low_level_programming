ea8f85c147af6fe2f33dcd8d4a6810d0b2e5a423
Bob Deprizio <robert.deprizio@gmail.com>
2019-11-05 23:56:15 +0000

    9. Insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-
 /**
  * insert_nodeint_at_index - function that inserts a new node at a given
  * position
@@ -13,6 +12,7 @@
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
+
 	unsigned int i = 0;
 	listint_t *new;
 	listint_t *temp;
@@ -47,6 +47,5 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 
 	free(new);
-
 	return (NULL);
 }
