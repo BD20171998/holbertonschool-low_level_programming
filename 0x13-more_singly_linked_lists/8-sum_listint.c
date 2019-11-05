3946d249c58db5d15fb0fec17a25c32dd29ab9e5
Bob Deprizio <robert.deprizio@gmail.com>
2019-11-05 21:18:08 +0000

    8. Sum list

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -10,11 +10,10 @@
 int sum_listint(listint_t *head)
 {
 	int sum = 0;
+	listint_t *temp;
 
 	if (head == NULL)
-		return (NULL);
-
-	listint_t *temp;
+		return (0);
 
 	for (temp = head; temp != NULL; temp = temp->next)
 		sum += temp->n;
