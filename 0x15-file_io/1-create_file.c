51be319a2d4a8d98d3b31008eae4a1753a52d2b0
Bob Deprizio <robert.deprizio@gmail.com>
2019-11-13 01:43:52 +0000

    1. Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,5 +1,5 @@
 #include "holberton.h"
-#include <stdio.h>
+
 /**
  * create_file - function that creates a file
  * @filename: Const char for name of file to be created
@@ -13,6 +13,9 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
+
+	if (text_content == NULL)
+		text_content = "";
 
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
