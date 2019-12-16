574acc34bb5fe53547de3be07dec1399172278cf
Bob Deprizio <robert.deprizio@gmail.com>
2019-12-16 01:57:51 +0000

    7. Insert at index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -109,7 +109,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 
 	len = length(*h);
-	if (idx == 0 || *h == NULL)
+
+	if (idx == 0)
 	{
 		new = add_dnodeint(h, n);
 		return (new);
@@ -132,9 +133,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		new = add_dnodeint_end(h, n);
 		return (new);
 	}
-
-	else if (idx > len)
-		return (NULL);
-
+	free(new);
 	return (NULL);
 }
