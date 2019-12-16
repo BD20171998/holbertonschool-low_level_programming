14ed7a5fcc10c553c1107fba3c6d2bf27626236c
Bob Deprizio <robert.deprizio@gmail.com>
2019-12-16 01:35:18 +0000

    8. Delete at index

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,66 +1,6 @@
 #include "lists.h"
 
 /**
- * length - function that gets number of nodes of for a linked dlistint_t list
- * @h: Pointer for linked dlistint_t list
- * Return: Unsigned int for length
- */
-
-unsigned int length(dlistint_t *h)
-{
-	dlistint_t *tem;
-	unsigned int l = 0;
-
-	tem = h;
-
-	while (tem != NULL)
-	{
-		++l;
-		tem = tem->next;
-	}
-
-	return (l);
-}
-
-/**
- * del_middle - function that deletes a middle node temp for a linked
- * dlistint_t list
- * @index: Index
- * @head: Pointer for linked dlistint_t list
- * Return: Void
- */
-
-void del_middle(dlistint_t *head, unsigned int index)
-{
-	dlistint_t *prev, *temp, *next;
-	unsigned int i = 0;
-
-	temp = head;
-	while (temp->next != NULL)
-	{
-		if (i == index)
-		{
-			prev = temp->prev;
-			next = temp->next;
-			prev->next = next;
-			next->prev = prev;
-			free(temp);
-			return;
-		}
-		if (temp->next == NULL)
-		{
-			prev = temp->prev;
-			prev->next = NULL;
-			free(temp);
-			return;
-		}
-		++i;
-		temp = temp->next;
-	}
-}
-
-
-/**
  * delete_dnodeint_at_index - function that deletes the node at "index" for
  * a dlistint_t list
  * @index: Index
@@ -70,22 +10,21 @@ void del_middle(dlistint_t *head, unsigned int index)
 
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *temp;
-	unsigned int len;
+	dlistint_t *prev, *temp, *next;
+	unsigned int i;
 
 	if (*head == NULL || head == NULL)
 		return (-1);
 
-	len = length(*head);
-
-	if (len == 1 && (*head)->next == NULL)
+	if (index == 0 && (*head)->next == NULL)
 	{
 		temp = *head;
 		*head = NULL;
 		free(temp);
 		return (1);
 	}
-	else if (index == 0 && len > 1)
+
+	else if (index == 0 && (*head)->next != NULL)
 	{
 		temp = *head;
 		*head = (*head)->next;
@@ -94,11 +33,17 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		return (1);
 	}
 
-	else if (index > 0 && len > 1)
+	for (i = 0, prev = *head; prev != NULL; i++, prev = prev->next)
 	{
-		del_middle(*head, index);
-		return (1);
+		if ((index - 1) == i)
+		{
+			temp = prev->next;
+			next = temp->next;
+			prev->next = next;
+			next->prev = prev;
+			free(temp);
+			return (1);
+		}
 	}
-
 	return (-1);
 }
