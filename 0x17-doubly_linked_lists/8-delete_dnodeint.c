2ee27ed5398eb770e2da9916544887a2221af426
Bob Deprizio <robert.deprizio@gmail.com>
2019-12-17 02:04:56 +0000

    8. Delete at index

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,6 +1,41 @@
 #include "lists.h"
 
 /**
+ * del_middle - Function that deletes a node outside of the head node
+ * @h: Pointer for linked dlistint_t list to head node
+ * @ind: Index
+ * Return: 1 if successed, -1 if failed
+ */
+
+int del_middle(dlistint_t *h, unsigned int ind)
+{
+	unsigned int i;
+	dlistint_t *prev, *temp, *next;
+
+	for (i = 0, prev = h; prev != NULL; i++, prev = prev->next)
+	{
+		if (prev->next == NULL)
+		{
+			temp = prev;
+			prev->prev->next = NULL;
+			free(temp);
+			return (1);
+		}
+
+		if ((ind - 1) == i)
+		{
+			temp = prev->next;
+			next = temp->next;
+			prev->next = next;
+			next->prev = prev;
+			free(temp);
+			return (1);
+		}
+	}
+	return (-1);
+}
+
+/**
  * delete_dnodeint_at_index - function that deletes the node at "index" for
  * a dlistint_t list
  * @index: Index
@@ -10,8 +45,8 @@
 
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *prev, *temp, *next;
-	unsigned int i;
+	dlistint_t *temp;
+	int j;
 
 	if (*head == NULL || head == NULL)
 		return (-1);
@@ -33,17 +68,11 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		return (1);
 	}
 
-	for (i = 0, prev = *head; prev != NULL; i++, prev = prev->next)
+	else if (index > 0)
 	{
-		if ((index - 1) == i)
-		{
-			temp = prev->next;
-			next = temp->next;
-			prev->next = next;
-			next->prev = prev;
-			free(temp);
-			return (1);
-		}
+		j = del_middle(*head, index);
+		return (j);
 	}
+
 	return (-1);
 }
