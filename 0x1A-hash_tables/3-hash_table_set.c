cbb3c1ed18069efb0d97155fff4df72a80738a86
Bob Deprizio <robert.deprizio@gmail.com>
2020-01-18 23:33:02 +0000

    3. >>> ht['betty'] = 'holberton'

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -50,7 +50,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (ht->array[index] == NULL)
 	{
 		ht->array[index] = new;
-		printf("Index is %lu\n", index);
 		return (1);
 	}
 
