e2e7c04c29bde51bb16aca71bbc881860bf73f22
Bob Deprizio <robert.deprizio@gmail.com>
2020-01-20 03:14:27 +0000

    3. >>> ht['betty'] = 'holberton'

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -60,6 +60,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		if (strcmp(temp->key, key) == 0)
 		{
+			free(ht->array[index]->value);
 			ht->array[index]->value = new->value;
 			free(new);
 			return (1);
